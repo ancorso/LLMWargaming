e46fd7d6b5e3f8f597bb77886e6d78d36a264e48
Max Lamparth <72386596+maxlampe@users.noreply.github.com>
2023-11-08 20:45:03 -0800

    feat: Added global config and simulation method.

--- run_game.jl
+++ run_game.jl
@@ -3,55 +3,86 @@ using DataFrames
 using CSV
 using Serialization
 
-# Prepare the connection to the GPT model
-secret_key = "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"
-model = "gpt-3.5-turbo-16k"
-chat_setup = ChatSetup(secret_key, model)
 
-# Setup the results dataframe and game dir
-wargame_dir = "wargame/"
-res = results_df(USPRCCrisisSimulation)
-boostrap_players = true
+# Struct for simulation config parameters
+@with_kw struct SimulationConfig
+    model::String = "gpt-3.5-turbo-16k"
+    secret_key::String = get(ENV, "OPENAI_API_KEY", "")
+    wargame_dir::String = "wargame/"
+    out_csv_file::String = "sample_results.csv"
+    boostrap_players::Bool = true
+    do_dialog::Bool = true # placeholder
+    verbose::Bool = false
+    save_results_to_csv::Bool = true
+    run_test_game::Bool = false
+    n_teams::Int = 10
+    n_players::Int = 6
+    n_dialog_steps::Int = 3 # placeholder
+end
 
-# Generate Treatments
-treatments = []
-for AI_accuracy in ["70-85%", "95-99%"]
-    for AI_system_training in [:basic, :significant]
-        for china_status in [:revisionist, :status_quo]
-            push!(treatments, USPRCCrisisSimulation(wargame_dir, AI_accuracy, AI_system_training, china_status))
+
+function run_simulation(config::SimulationConfig)
+
+    # Prepare the connection to the GPT model
+    if config.secret_key == ""
+        @warn "OPENAI_API_KEY not set in ENV"
+    end
+    chat_setup = ChatSetup(config.secret_key, config.model)
+
+    # Setup the results dataframe and game dir
+    res = results_df(USPRCCrisisSimulation)
+
+    # Generate Treatments
+    treatments = []
+    for AI_accuracy in ["70-85%", "95-99%"]
+        for AI_system_training in [:basic, :significant]
+            for china_status in [:revisionist, :status_quo]
+                push!(treatments, USPRCCrisisSimulation(config.wargame_dir, AI_accuracy, AI_system_training, china_status))
+            end
         end
     end
-end
 
-# Generate one team (for now)
-if boostrap_players
-    loaded_player_data = deserialize("wargame/player_data.jls")
-    teams = [[rand(loaded_player_data) for i in 1:6] for i=1:10]
-else
-    teams = [[Player() for i in 1:6] for i=1:10]
-end
+    # Generate teams
+    if config.boostrap_players
+        loaded_player_data = deserialize(config.wargame_dir * "player_data.jls")
+        teams = [[rand(loaded_player_data) for i in 1:config.n_players] for i=1:config.n_teams]
+    else
+        teams = [[Player() for i in 1:config.n_players] for i=1:config.n_teams]
+    end
 
-# # Print a sample:
-# print_prompts(treatments[1], team)
+    # Run a test game or all treatments * teams
+    if config.run_test_game
+        result = run_game(treatments[1], teams[1], chat_setup, verbose=config.verbose)
+        push!(res, result)
 
-# # Run a sample game
-# result = run_game(treatments[1], team, chat_setup, verbose=true)
+        # Write the results DataFrame to a CSV file
+        if config.save_results_to_csv
+            CSV.write("results/" * config.out_csv_file, res)
+        end
+    else
+        # Run the games
+        for (j, team) in enumerate(teams)
+            println("Team: ", j)
+            for (i, game) in enumerate(treatments)
+                println("Running treatment: ", i)
+                try 
+                    result = run_game(game, team, chat_setup)
+                    push!(res, result)
 
-# Run the games
-for (j,team) in enumerate(teams)
-    println("Team: ", j)
-    for (i, game) in enumerate(treatments)
-        println("Running treatment: ", i)
-        try 
-            results = run_game(game, team, chat_setup)
-            push!(res, results)
-            CSV.write("results/v1_results.csv", res)
-        catch e
-            println(e)
-            sleep(10)
+                    # Write the results DataFrame to a CSV file
+                    if config.save_results_to_csv
+                        CSV.write("results/" * config.out_csv_file, res)
+                    end
+                catch e
+                    println(e)
+                    sleep(10)
+                end
+            end
         end
     end
+
+    return res
 end
 
-# Write the results DataFrame to a CSV file
-CSV.write("results/sample_results.csv", res)
+# run_simulation(SimulationConfig())
+run_simulation(SimulationConfig(run_test_game=true))
